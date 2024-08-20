08d5ad79bd2d7863131b5848efb0a3883a0b1476
Florent <148872635+Froggys974@users.noreply.github.com>
2024-08-20 23:57:50 +0200

    Revert "Add api for f1"

--- api/routes/timerRoute.js
+++
@@ -1,9 +0,0 @@
-module.exports = (server) => {
-    const timerController = require('../controllers/timerController');
-    const { authenticateToken } = require('../middlewares/jwtMiddleware');
-
-    server.route('/submit-reaction-time')
-    .post(timerController.submitReactionTime);
-    server.route('/get-reaction-times/:userId')
-    .get(timerController.getReactionTimes);
-};
